7627d1c6f72ea764bb78824405a54084962d48d6
Shaina Peters <darthbeep@gmail.com>
2023-05-28 07:27:40 -0400

    source filtering and ui improvements

--- js/5etools-importer.js
+++ js/5etools-importer.js
@@ -705,7 +705,7 @@ function d20plusImporter () {
 
 		// init list library
 		const importList = new List("import-list", {
-			valueNames: options.listIndex || ["name"],
+			valueNames: options.listIndex || ["name", "source"],
 		});
 
 		// reset the UI and add handlers
@@ -730,8 +730,11 @@ function d20plusImporter () {
 			d20plus.importer._importSelectPublished(importList);
 		});
 
-		$("#importlist-filter-list").bind("click", () => {
+		$("#importlist-filter").bind("click", () => {
 			d20plus.importer._importFilterList(importList);
+		});
+		$("#importlist-reset").bind("click", () => {
+			d20plus.importer._importResetList(importList);
 		});
 
 		if (options.listIndexConverter) {
@@ -1224,21 +1227,51 @@ function d20plusImporter () {
 		const $winText = $winFilterList.find(".table-import-textarea");
 		
 		$btnImport.on("click", () => {
-			const toSearch = $winText.val().toLowerCase().split("\n");
 
-			// If no search terms are entered, reset the filter
-			if (toSearch.length == 1 && toSearch[0] == '') {
-				importList.filter();
-			}
-			// Filters to match names on the list
-			else{
+			function filterList () {
+				const toSearch = $winText.val().toLowerCase().replaceAll(`"`, "").split("\n");
+				const firstLine = toSearch[0];
+				const filterUnofficial = !d20plus.cfg.getOrDefault("import", "allSourcesIncludeUnofficial");
+
+				// If no search terms are entered, reset the filter
+				if (toSearch.length == 1 && firstLine == '') {
+					importList.filter();
+					return;
+				}
+
+				const searchDict = {};
+				toSearch.forEach(it => {
+					const items = it.split(",");
+					// If additional filters are added, they can go here
+					searchDict[items[0].trim()] = {
+						"name": items[0].trim(),
+						"source": items.length > 1 ? items[1].trim() : null,
+						// Some categories list their source in this format
+						"altsource": items.length > 1 ? `src[${items[1].trim()}]`: null,
+					}
+				})
+
+				// Filters to match names and sources on the list
+				x = importList
 				importList.filter(it => {
-					return toSearch.includes(it._values.name.toLowerCase());
+					const name = it._values.name.toLowerCase();
+					const source = it._values.source.toLowerCase();
+					
+					if (!(name in searchDict)) return false;
+					if (!searchDict[name].source) return true;
+					if (searchDict[name].source === source || searchDict[name].altsource === source) return true;
+					return false;
 				});
 			}
 			
+			filterList();
+			
 			$winFilterList.dialog("close");
 		}).appendTo($winFilterList);
+	};
+
+	d20plus.importer._importResetList = function (importList) {
+		importList.filter();
 	};
 
 	d20plus.importer.CharacterAttributesProxy = class {
--- js/5etools-template.js
+++ js/5etools-template.js
@@ -228,7 +228,7 @@ const d20plusTemplate = function () {
 			autoOpen: false,
 			resizable: true,
 			width: 640,
-			height: 480,
+			height: 540,
 		});
 
 		// add class subclasses to the subclasses dropdown(s)
@@ -329,7 +329,8 @@ ${finalText}
 <span style="width:1px;background: #bbb;height: 26px;margin: 2px;"></span>
 <button type="button" id="importlist-selectall-published" class="btn" style="margin: 0 2px;"><span>Select All Published</span></button>
 <span style="width:1px;background: #bbb;height: 26px;margin: 2px;"></span>
-<button type="button" id="importlist-filter-list" class="btn" style="margin: 0 2px;"><span>Filter By List</span></button>
+<button type="button" id="importlist-filter" class="btn" style="margin: 0 2px;"><span>Filter By List</span></button>
+<button type="button" id="importlist-reset" class="btn" style="margin: 0 2px;"><span>Reset Filters</span></button>
 </p>
 <p>
 <span id="import-list">
@@ -363,10 +364,27 @@ ${finalText}
 </div>`;
 
 d20plus.template5e.importFilterList = `<div id="d20plus-import-filter-list" title="List Items to Filter For">
+<p>
+	Write down a vertical list of the items you want to filter and then <b>Filter List > Select Visible > Import</b>
+	<span title="If an item is in more than one source (like those that get reprinted or appear in UAs), you will get multiple results out of the filter for it unless you indicate a source." style="cursor: help;">[!]</span>.
+</p>
+<p>
+	You can either manually write down your list, or you can go to the corresponding page in <b>5etools > make your list > Table View > Copy CSV to Clipboard > Paste it here > Filter List</b>
+	<span title="Don't worry if there is more info that is needed, it'll get discarded, as well as any failed match. Caps are optional as well." style="cursor: help;">[?]</span>.
+</p>
 <textarea placeholder=
-"Item 1\nItem 2\nItem 3" class="table-import-textarea">
+'List 1 item per line. There are 3 ways of listing things (you can combine them):
+
+Fireball
+Fireball, PHB
+"Fireball","PHB"
+
+etc.
+(Source is optional, as you can see in the 1st example, but it is recommended)
+(the 2nd and 3rd example show the 2 ways to indicate source)'
+class="table-import-textarea">
 </textarea>
-<button class="btn">Import</button>
+<button class="btn">Filter</button>
 </div>`;
 
 	d20plus.template5e.importDialogHtml = `<div id="d20plus-import" title="Importing">
